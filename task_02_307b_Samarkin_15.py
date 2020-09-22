2e1d9ac11542c7e55db0352f0c9ef4acc1d396c2
Nik Sam <work.nicitons@yandex.ru>
2020-09-22 22:11:00 +0300

    Added file operations to write result

--- task_02_307b_Samarkin_15.py
+++ task_02_307b_Samarkin_15.py
@@ -1,7 +1,7 @@
 from scipy.constants import c, pi
 from scipy.special import spherical_jn as jn, spherical_yn as yn
 import numpy as np
-import pathlib as pth
+from pathlib import Path as pth
 import matplotlib.pyplot as plt
 
 #Тестовые переменные
@@ -18,23 +18,36 @@ def hn(n, x):
 def bn(n, x):
     upr_n = x * jn(n-1, x) - n*jn(n, x)
     lwr_n = x * hn(n-1, x) - n * hn(n, x)
-    if lwr_n != 0: return upr_n / lwr_n
-    else: return 0
+    return upr_n / lwr_n
 
 def an(n, x):
-    if hn(n, x) != 0: return jn(n, x) / hn(n, x)
-    else: return 0 
+    return jn(n, x) / hn(n, x) 
 
 def f_sigma(x):
     lmbd = c / x #Лямбда
     k = 2 * pi / lmbd #Волновое число
     kr = k*r
     Sum = 0+0j
-    for n in range(1, 40):
-        Sum += (-1 ** n) * (n+ 1/2) * (bn(n, kr) - an(n, kr))
-    return ((lmbd**2) / pi) * (np.abs(Sum)**2)
+    for n in range(1, 45):
+        Sum += (-1 ** n) * (n + 1/2) * (bn(n, kr) - an(n, kr))
+    return ((lmbd**2) / pi) * (np.abs(Sum)**2) 
 
 sigma = [f_sigma(x) for x in f]
+
+p = pth('results')
+res = p / 'task_02_307b_Samarkin_15.txt'
+if not p.exists():
+  p.mkdir(exist_ok = True)
+
+if p.exists():
+  with res.open('w') as fl:
+    i = 0
+    for x in f:
+      fl.write('%11.10f    %.10f\n' % (x, sigma[i]))
+      i += 1
+else:
+  print("Error: \'results\' dir does not exists - access denied")
+
 
 plt.plot(f/1e9, sigma)
 plt.xlabel('f, ГГц')
